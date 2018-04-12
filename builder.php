ba9db4620cc77bdc3b84855e321c3d8cef6cdc01
Anton <wrugleys@yandex.ru>
2018-04-13 00:04:47 +0300

    Final chart 1

--- builder.php
+++ builder.php
@@ -3,35 +3,68 @@
 
 function buildHeader($data) : string
 {
-   $data = strtoupper($data);
+    $d = colorizeWeb(strtoupper($data), 'blue');
 
-  return printf("<span style='color: red'>$data</span>");
+  return $d;
 
 
 
 };
 
-function buildContent($data) : string
+function buildContent($data)
 {
-$d = ' ';
 
+$d='';
     foreach ($data as $line) {
-        $label = isset($line['label']) ? printf("<span style='color: green'>$data</span>") : '';
-        switch ($line['type']) {
-            case 'skill':
-                $d .= $label . implode(', ', $line['data']) . PHP_EOL;
-                break;
-            case 'list':
-                unset($line['type']);
-                foreach ($line as $key => $value) {
-                    $list = is_array($value) ? implode(', ', $value) : $value;
-                    $d .=  printf("<span style='color: green'> $key </span>") . $list . PHP_EOL;
+
+
+                $label = isset($line['label']) ? $line['label'] : '';
+                switch ($line['type']) {
+                    case 'item':
+                        $d .= '<b>'.colorizeWeb($label, 'red').'</b>'. ': '. implode(', ', $line['data']). "</br>";
+                        break;
+                    case 'list':
+                        unset($line['type']);
+
+                        foreach ($line as $key => $value) {
+                            $list = is_array($value) ? implode(', ', $value) : $value;
+                            $d .= $key .': '. $list . "</br>";
+                        }
+                        break;
+                    default:
+                         if ($label == '') {
+                        $d .= '<b>'.colorizeWeb($label, 'green').'</b>'.  $line['data']. "</br>";
+                         }
+                    else  { $d .= '<b>'.colorizeWeb($label, 'green').'</b>'.': '. $line['data']. "</br>"; }
+
                 }
-                $d .= PHP_EOL;
-                break;
-            default:
-                $d .= $label . $line['data'] . PHP_EOL;
-        }
-    }
+
+            }
+
     return $d;
+    };
+
+function colorizeWeb(string $text, string $color): string {
+
+    return '<span style="color:' . $color .'">' . $text . '</span>';
+}
+
+function colorize(string $text, string $color): string {
+    switch($color) {
+        case 'green':
+            $out = "[42m"; //Green background
+            break;
+        case 'red':
+            $out = "[41m"; //Red background
+            break;
+        case 'yellow':
+            $out = "[43m"; //Yellow background
+            break;
+        case 'blue':
+            $out = "[44m"; //Blue background
+            break;
+        default:
+            return $text;
+    }
+    return chr(27) . "$out" . "$text" . chr(27) ;
 };
